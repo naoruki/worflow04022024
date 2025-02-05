6840ef558f5010357a43fce20445737dd222c564
naoruki <72594954+naoruki@users.noreply.github.com>
2025-02-05 09:42:59 +0800

    create s3 bucket

--- backend.tf
+++ backend.tf
@@ -1,10 +1,19 @@
 provider "aws" {
   region = "ap-southeast-1"
 }
-terraform {
-  backend "s3" {
-    bucket = "sctp-ce8-tfstate"
-    region = "ap-southeast-1"
-    key = "cornelia-activity.tfstate" # must be different from other projects
+# terraform {
+#   backend "s3" {
+#     bucket = "sctp-ce8-tfstate"
+#     region = "ap-southeast-1"
+#     key = "cornelia-activity.tfstate" # must be different from other projects
+#   }
+# }
+# Create an S3 bucket
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "cornelia-s3-bucket" # Replace with your desired bucket name
+
+  tags = {
+    Name        = "CorneliaS3Bucket"
+    Environment = "Production"
   }
 }
